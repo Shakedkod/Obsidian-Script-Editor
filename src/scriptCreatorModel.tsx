b23fd97eaa5386a2143ae0cb3c334ae7b5d979f6
ShakedKod <ShakedKod@gmail.com>
2025-06-18 23:18:08 +0300

    some fixes

--- src/scriptCreatorModel.tsx
+++ src/scriptCreatorModel.tsx
@@ -4,41 +4,69 @@ import { useState } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 type Props = {
-    onSubmit: (name: string) => void;
+    onSubmit: (name: string, author: string, prodCompany: string, date: string) => void;
     onClose: () => void;
 };
 
 export const ScriptNameModalContent: React.FC<Props> = ({ onSubmit, onClose }) => {
     const [name, setName] = useState("");
+    const [author, setAuthor] = useState("");
+    const [prodCompany, setProdCompany] = useState("");
+    const [date, setDate] = useState(new Date().toISOString().split('T')[0]); // Default to today's date
 
     const handleSubmit = () => {
         if (name.trim()) {
-            onSubmit(name.trim());
+            onSubmit(name.trim(), author.trim(), prodCompany.trim(), date.trim());
         }
     };
 
     return (
         <div style={{"padding":"0.5rem"}}>
-            <h2 style={{"marginBottom":"2rem","fontSize":"1.125rem","lineHeight":"1.75rem","fontWeight":700}}>
+            <h2 style={{"marginBottom":"2rem","fontSize":"1.125rem","lineHeight":"1.75rem","fontWeight":700,textAlign:"center"}}>
                 New Script
             </h2>
             <input
                 type="text"
-                style={{"padding":"0.5rem","marginBottom":"1rem","borderRadius":"0.25rem","borderWidth":"1px","width":"100%","color":"#000000"}}
+                style={{"padding":"0.5rem","marginBottom":"1rem","borderRadius":"0.25rem","borderWidth":"1px","width":"100%"}}
                 placeholder="Enter script name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
             />
-            <button
-                onClick={handleSubmit}
-                style={{"paddingTop":"0.25rem","paddingBottom":"0.25rem","paddingLeft":"0.75rem","paddingRight":"0.75rem","marginRight":"0.5rem","borderRadius":"0.25rem","color":"#ffffff","backgroundColor":"#2563EB"}}
-            >
-                Create
-            </button>
-            <button onClick={onClose} style={{"paddingTop":"0.25rem","paddingBottom":"0.25rem","paddingLeft":"0.75rem","paddingRight":"0.75rem","color":"#D1D5DB"}}>
-                Cancel
-            </button>
+            <input
+                type="text"
+                style={{"padding":"0.5rem","marginBottom":"1rem","borderRadius":"0.25rem","borderWidth":"1px","width":"100%"}}
+                placeholder="Enter script author"
+                value={author}
+                onChange={(e) => setAuthor(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
+            />
+            <input
+                type="text"
+                style={{"padding":"0.5rem","marginBottom":"1rem","borderRadius":"0.25rem","borderWidth":"1px","width":"100%"}}
+                placeholder="Enter production company"
+                value={prodCompany}
+                onChange={(e) => setProdCompany(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
+            />
+            <input
+                type="date"
+                style={{"padding":"0.5rem","marginBottom":"1rem", "paddingLeft":"1.5rem","borderRadius":"0.25rem","borderWidth":"1px","width":"100%"}}
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && handleSubmit()}
+            />
+            <div style={{"display":"flex","justifyContent":"center","gap":"0.5rem"}}>
+                <button
+                    onClick={handleSubmit}
+                    style={{"paddingTop":"0.25rem","paddingBottom":"0.25rem","paddingLeft":"0.75rem","paddingRight":"0.75rem","marginRight":"0.5rem","borderRadius":"0.25rem","color":"#ffffff","backgroundColor":"#2563EB"}}
+                >
+                    Create
+                </button>
+                <button onClick={onClose} style={{"paddingTop":"0.25rem","paddingBottom":"0.25rem","paddingLeft":"0.75rem","paddingRight":"0.75rem","color":"#D1D5DB"}}>
+                    Cancel
+                </button>
+            </div>
         </div>
     );
 };
@@ -46,9 +74,9 @@ export const ScriptNameModalContent: React.FC<Props> = ({ onSubmit, onClose }) =
 class ScriptNameModal extends Modal 
 {
     private root: ReactDOM.Root;
-    onSubmit: (value: string) => void;
+    onSubmit: (name: string, author: string, prodCompany: string, date: string) => void;
 
-    constructor(app: App, onSubmit: (value: string) => void) {
+    constructor(app: App, onSubmit: (name: string, author: string, prodCompany: string, date: string) => void) {
         super(app);
         this.onSubmit = onSubmit;
     }
@@ -57,8 +85,8 @@ class ScriptNameModal extends Modal
         this.root = ReactDOM.createRoot(this.contentEl);
         this.root.render(
             <ScriptNameModalContent
-                onSubmit={(name) => {
-                    this.onSubmit(name);
+                onSubmit={(name: string, author: string, prodCompany: string, date: string) => {
+                    this.onSubmit(name, author, prodCompany, date);
                     this.close();
                 }}
                 onClose={() => this.close()}
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    content: ["./**/*.{tsx,ts}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-};
